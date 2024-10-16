1b203e0900d60637277f8abb6172fc6caf398daf
cornel12-hen <noelmarvell6@gmail.com>
2024-10-16 10:27:29 +0700

    add index dan home page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return ('Tentang Que');
 });
