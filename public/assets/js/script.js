dcdbc142175b7e8adeba92f7101ccf8e87beb452
cornel12-hen <noelmarvell6@gmail.com>
2024-11-06 11:32:24 +0700

    Filament On Progress

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,18 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-  event.preventDefault();
+document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault();
 
-  const correctUsername = "colnel";
-  const correctPassword = "1234";
+        const correctUsername = "colnel";
+        const correctPassword = "1234";
 
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
 
-  if (username === correctUsername && password === correctPassword) {
-    alert("Login Berhasil");
-    window.location.href = "home";
-  } else {
-    alert("Username atau Password salah");
-  }
-});
+        if (username === correctUsername && password === correctPassword) {
+            alert("Login Berhasil");
+            window.location.href = "/";
+        } else {
+            alert("Username atau Password salah");
+        }
+    });
